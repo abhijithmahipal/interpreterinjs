f84a87d984d926ae5e818a3a4e4e7f718618fbee
abhijithmahipal <abhijithmahipal@gmail.com>
2017-09-26 01:15:01 +0530

    parsing completed

--- schemeinjs.js
+++ schemeinjs.js
@@ -1,6 +1,35 @@
-var program='(begin (define r 10)(* pi (* r r)))';
-var tokens=function tokenize(chars){
+var program='(begin (define r 10) (* pi (* r r)))';
+var tokenize=function(chars){
 	return chars.replace(/[(]/g,' ( ').replace(/[)]/g,' ) ').trim().split(/ +/);
+}
+
+var parse=function(program){
+	return read_from_tokens(tokenize(program));
+}
+
+var read_from_tokens=function(tokens){
+	if(tokens.length == 0){
+		throw('Syntax Error : unexpected EOF while reading');
+		return;
+	}
+	token=tokens.shift();
+	if(token=='('){
+		var l=[];
+		while(tokens[0]!=')'){
+			l.push(read_from_tokens(tokens));
+		}
+		tokens.shift();
+		return l;
+	}
+	else if(token==')'){
+		throw('Syntax Error : unexpected');
+		return;
+	}
+	else{
+		return token;
+	}
+
+		
 }
 
 
@@ -8,7 +37,23 @@ var tokens=function tokenize(chars){
 
 
 
-console.log(tokens(program));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(parse(program));
 //console.log()
 //console.log()
 
