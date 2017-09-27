3fa3bc2b86c3922f8459849a920f2ad70c822b81
abhijithmahipal <abhijithmahipal@gmail.com>
2017-09-27 15:36:20 +0530

    added arithmetic operator, corrected mistakes in previous version.

--- schemeinjs.js
+++ schemeinjs.js
@@ -1,4 +1,4 @@
-var program='(* 480 55)';
+var program='(>= 4 6 )';
 var tokenize=function(chars){
 	return chars.replace(/[(]/g,' ( ').replace(/[)]/g,' ) ').trim().split(/ +/);
 }
@@ -31,39 +31,54 @@ var read_from_tokens=function(tokens){
 }
 
 var atom=function(token){
-  if(isNaN(Number(token))){
-    return String(token);
-  }
-   else
-     return Number(token);
+	if(isNaN(Number(token))){
+    		return String(token);
+  	}
+   	else
+     		return Number(token);
 }
 
 var std_env=function(){
-  function mul(a,b){return a*b;};
-  
-  env={
-    '*': mul
-  };
-  return env;
+	function mul(a,b){return a*b;};
+	function add(a,b){return a+b;};
+	function sub(a,b){return a-b;};
+	function div(a,b){return a/b;};
+	function gt(a,b){return a>b;};
+  	function lt(a,b){return a<b;};
+  	function ge(a,b){return (a>b && a==b);}
+	function le(a,b){return (a<b && a==b);}
+	function eq(a,b){return a==b;}
+
+  	env={
+    		'*': mul,
+		'+': add,
+		'-': sub,
+		'/': div,
+		'<': lt,
+		'>': gt,
+		'<=':le,
+		'>=':ge,
+		'=':eq
+  	};
+  	return env;
 }
 
 global_env=std_env();
 
 var eval=function(x,env){
-  env=global_env;
-  if(typeof x == 'string')
-     return env[x];
-  else if(typeof x == 'number')
-     return x;
-  else{
-    proc = eval(x[0],env);
-    var args=[];
-    console.log(x[2]);
-    for(i=1;i<x.length;i++)
-      args.push(eval(x[i],env));
-   	console.log(args);
-	return proc.apply(this,args);
-  }
+	env=global_env;
+  	if(typeof x == 'string')
+     		return env[x];
+  	else if(typeof x == 'number')
+     		return x;
+  	else{
+    		var proc = eval(x[0],env);
+    		var args=[];
+		for(var i=1;i<x.length;i++)		
+      			args.push(eval(x[i],env));
+		console.log(args);
+		return proc.apply(this,args);
+  	}
 }
 
 
