915afa25c024def1a96f787898be45f65ff0d451
abhijithmahipal <abhijithmahipal@gmail.com>
2017-09-30 23:42:54 +0530

    basic operations over

--- schemeinjs.js
+++ schemeinjs.js
@@ -1,4 +1,6 @@
-var program='(>= 4 6 )';
+var p0='(define a (list 1 2 3 4 5))';
+var p1='(define b (> 1 2))';
+var p2='(cons 2 a )';
 var tokenize=function(chars){
 	return chars.replace(/[(]/g,' ( ').replace(/[)]/g,' ) ').trim().split(/ +/);
 }
@@ -45,12 +47,19 @@ var std_env=function(){
 	function div(a,b){return a/b;};
 	function gt(a,b){return a>b;};
   	function lt(a,b){return a<b;};
-  	function ge(a,b){return (a>b && a==b);}
-	function le(a,b){return (a<b && a==b);}
-	function eq(a,b){return a==b;}
+  	function ge(a,b){return (a>b && a==b);};
+	function le(a,b){return (a<b && a==b);};
+	function eq(a,b){return a==b;};
+	function append(a,b){return a.concat(b);};
+	function car(a){return a[0]};
+	function cdr(a){return a.slice(1)};
+	function cons(a, b){b.unshift(a);return b;};
+	function len(a){return a.length;};
+	function listcheck(a){return (typeof a)=='object';};
+	function not(a){return !a;};
 
   	env={
-    		'*': mul,
+    	'*': mul,
 		'+': add,
 		'-': sub,
 		'/': div,
@@ -58,7 +67,28 @@ var std_env=function(){
 		'>': gt,
 		'<=':le,
 		'>=':ge,
-		'=':eq
+		'=':eq,
+		'abs':Math.abs,
+		'append':append,
+		'list':Array,
+		'car':car,
+		'cdr':cdr,
+		'cons':cons,
+		'eq?':eq,
+		'equal?':eq,
+		'length':len,
+		'list?':listcheck,
+		'not':not,
+		'min':Math.min,
+		'max':Math.max,
+		'round':Math.round,
+		'sin':Math.sin,
+		'cos':Math.cos,
+		'tan':Math.tan,
+		'sqrt':Math.sqrt,
+		'pow':Math.pow,
+		'log':Math.log
+
   	};
   	return env;
 }
@@ -71,17 +101,34 @@ var eval=function(x,env){
      		return env[x];
   	else if(typeof x == 'number')
      		return x;
+	else if(x[0]=='if'){
+		var test = x[1];
+		var conseq=x[2];
+		var alt=x[3];
+		if(eval(test,env))
+			exp=conseq;
+		else
+			exp=alt;
+		return eval(exp,env);
+	}
+	else if(x[0]=='define'){
+		var vari=x[1];
+		var exp=x[2];
+		env[vari]=eval(exp,env);
+		return 'Variable Added';
+	}
+	
+
   	else{
     		var proc = eval(x[0],env);
     		var args=[];
 		for(var i=1;i<x.length;i++)		
       			args.push(eval(x[i],env));
-		console.log(args);
 		return proc.apply(this,args);
   	}
 }
 
 
-console.log(parse(program));
-console.log(eval(parse(program)));
-
+console.log(eval(parse(p0)));
+console.log(eval(parse(p1)));
+console.log(eval(parse(p2)));
